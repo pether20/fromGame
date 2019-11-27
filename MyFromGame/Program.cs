ebd5506b44388265d5453dcf66aae455da799c72
Pether Tejada <pether.te.z@hotmail.com>
2019-11-26 22:00:42 -0500

    add codigo visual

--- MyFromGame/Program.cs
+++ MyFromGame/Program.cs
@@ -7,6 +7,30 @@ using System.Threading.Tasks;
 namespace MyFromGame {
     class Program {
         static void Main(string[] args) {
+
+            int x = 5;
+            float y = 4.5f;
+            string str = "Ludimatizando";
+            bool mybool = false;
+
+            float result;
+
+            result = 5 + 7;
+            Console.WriteLine("suma"+result);
+
+            result = 5 - 7;
+            Console.WriteLine("resta" + result);
+
+            result = 5 * 7;
+            Console.WriteLine("mult" + result);
+
+            result = 5f / 7f;
+            Console.WriteLine("div" + result);
+
+            result = 5 % 2;
+            Console.WriteLine("res" + result);
+
+            Console.ReadLine();
         }
     }
 }
